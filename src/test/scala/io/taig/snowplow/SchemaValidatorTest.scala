847220f9e699aa3b8e081bf2c339fbb6e4cbdf5e
Niklas Klein <mail@taig.io>
2019-08-11 15:28:24 +0200

    Add test case for null JSON values

--- src/test/scala/io/taig/snowplow/SchemaValidatorTest.scala
+++ src/test/scala/io/taig/snowplow/SchemaValidatorTest.scala
@@ -29,4 +29,17 @@ trait SchemaValidatorTest { this: FlatSpec =>
 
     assert(result.unsafeRunSync().nonEmpty)
   }
+
+  it should "ignore null values" in {
+    val result = for {
+      validator <- validator
+      schema <- resource("config-schema.json")
+      config <- resource("config.json")
+      configNulls <- resource("config-nulls.json")
+      result <- validator.validate(schema, config)
+      resultNulls <- validator.validate(schema, configNulls)
+    } yield result == resultNulls
+
+    assert(result.unsafeRunSync())
+  }
 }
